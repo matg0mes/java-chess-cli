6eb7c182049c93b8cf2d8c75fe99cc439ea2dffb
FoxGreedy <hotmus8500@gmail.com>
2019-09-28 13:22:48 -0300

    Board Created and pieces configuration

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,13 @@
 package aplication;
 
+import Chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 		
 	}
 
